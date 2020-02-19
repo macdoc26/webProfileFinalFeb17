76f0b752610fe29d779813cc8dab0d2eec088a33
macdoc26 <60278653+macdoc26@users.noreply.github.com>
2020-02-19 15:37:27 -0500

    requirements file

--- personalProfile/personalProfile/settings.py
+++ personalProfile/personalProfile/settings.py
@@ -76,6 +76,9 @@ WSGI_APPLICATION = 'personalProfile.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -86,7 +89,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
