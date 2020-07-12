997e0e040ba84bf0f482c5d6254db6cd00459e1a
macdoc26 <60278653+macdoc26@users.noreply.github.com>
2020-07-12 11:55:04 -0400

    Update wsgi.py

--- personalProfile/personalProfile/wsgi.py
+++ personalProfile/personalProfile/wsgi.py
@@ -12,6 +12,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalProfile.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalProfile.personalProfile.settings')
 
 application = get_wsgi_application()
